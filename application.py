57dd0f1ad41da266e299848950f8a349013e9670
Mykola Ptashnik <33723397+htmlPHPmysql@users.noreply.github.com>
2025-08-15 13:30:52 +0000

    add main() function to application.py

--- application.py
+++ application.py
@@ -1,1 +1,10 @@
-imprt streamlit as st
+import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title="Ask question about yuor PDFs")
+    st.header("PDF Document Assistant")
+    st.chat_input("Ask question about yuor PDF...")
+    with st.sidebar:
+        st.subheader("Yuor documents")
+        
