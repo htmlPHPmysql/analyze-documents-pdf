083ca44aca3cb3598e629be6208830643b4ab5a4
Mykola Ptashnik <33723397+htmlPHPmysql@users.noreply.github.com>
2025-08-15 14:26:26 +0000

    add get_pdf_text function in application.py

--- application.py
+++ application.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pdfplumber
+
+def get_pdf_text(pdf_document):
+    text = ""
+    for pdf in pdf_document:
+        with pdfplumber.open(pdf) as pdf_reader:
+            for page in pdf_reader.pages:
+                text += page.extract_text() or ""
+    return text
 
 
 def main():
@@ -7,4 +16,22 @@ def main():
     st.chat_input("Ask question about yuor PDF...")
     with st.sidebar:
         st.subheader("Yuor documents")
-        
+        pdf_documents = st.file_uploader(
+            "Upload your documents and click on 'Process'", accept_multiple_files=True
+        )
+        if st.button("Process"):
+            if not pdf_documents:
+                st.error("Upload at least one doc")
+                return
+
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_documents)
+                st.write(raw_text)
+
+                # get the text chunks
+
+                # create vector store
+
+                # create conversation chian
+                st.success(f"Documents {len(pdf_documents)} processed successfully")
